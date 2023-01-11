7619aa3b83f1f13ea389a9b005b3a9db98da8700
Thomas K <thmskffr@gmail.com>
2023-01-11 17:16:17 +0100

    21 done, could benefit from some refactoring

--- 21_todolist.js
+++ 21_todolist.js
@@ -8,13 +8,20 @@ taskList.style.width = "100%";
 taskNameForm.onsubmit = (e) => {
   e.preventDefault();
   let taskCard = document.createElement("div");
-  taskCard.style = Object.assign(taskCard.style, {
-    backgroundColor: "lightgrey",
-    border: "solid 2px black",
-    width: "100%",
-  });
+  taskCard.style.width = "80%";
+  taskCard.style.backgroundColor = "lightgrey";
+  taskCard.style.border = "solid 2px black";
+  taskCard.style.borderRadius = "8px";
+  taskCard.style.padding = "8px";
   const taskName = document.createElement("h3");
   taskName.innerHTML = taskNameInput.value;
   taskCard.append(taskName);
   taskList.append(taskCard);
+  console.dir(taskCard);
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+  deleteButton.addEventListener("click", () => {
+    taskCard.remove();
+  });
+  taskCard.appendChild(deleteButton);
 };
