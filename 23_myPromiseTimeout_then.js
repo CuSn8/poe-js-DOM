71c6fb3e819b62978d48ccf6218155ef74e14661
Thomas K <thmskffr@gmail.com>
2023-01-12 11:21:52 +0100

    23 done

---
+++ 23_myPromiseTimeout_then.js
@@ -0,0 +1,15 @@
+const body = document.getElementsByTagName("body")[0];
+
+const addButton = document.getElementsByTagName("button")[0];
+
+const sleep = (milliseconds) => {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
+
+addButton.addEventListener("click", () => {
+  sleep(2000).then(() => {
+    const addMe = document.createElement("p");
+    addMe.innerText = "Added !";
+    body.append(addMe);
+  });
+});
